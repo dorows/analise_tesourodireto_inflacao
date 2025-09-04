e9503ae7414d0a71d0c153dce5a6b96b7a758627
dorows <gustavosdorow@gmail.com>
2025-09-04 10:42:39 -0300

    commit

--- main.R
+++ main.R
@@ -1,1 +1,11 @@
-read.csv2("precotaxatesourodireto.csv")
+base1 <- read.csv2("precotaxatesourodireto.csv")
+
+library(dplyr)
+library(lubridate)
+
+base1 <- base1 %>%
+  mutate(Data.Base = dmy(Data.Base)) %>% 
+  filter(year(Data.Base) >= 2022)       
+
+head(base1)
+
